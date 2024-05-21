9eda9243bae4009c5b1e1f2808481e5be16325a6
cocolo93 <chococolo0903@gmail.com>
2024-05-21 15:05:29 +0900

    Week2-Homework

---
+++ Week2-Homework/cache.py
@@ -0,0 +1,155 @@
+import sys
+
+# Implement a data structure that stores the most recently accessed N pages.
+# See the below test cases to see how it should work.
+#
+# Note: Please do not use a library like collections.OrderedDict). The goal is
+#       to implement the data structure yourself!
+
+# Hash function.
+#
+# |key|: string
+# Return value: a hash value
+def calculate_hash(url):
+    assert type(url) == str
+    # Note: This is not a good hash function. Do you see why?
+    hash = 0
+    index = 1
+    for i in url:
+        hash += ord(i) * index
+        index *= 37
+    return hash
+
+class Webpage:
+    def __init__(self, url, contents, next):
+        self.url = url
+        self.content = contents
+        self.next = next
+
+class Cache:
+    def __init__(self, n):
+        self.max_cache = n
+        self.cache = []
+        self.bucket_size = 97
+        self.buckets = [None] * self.bucket_size
+
+    # Check if the URL exists in the cache.
+    # Update cache if it exists and return True.
+    # If not, return False.
+    def update(self, url):
+        bucket_index = calculate_hash(url) % self.bucket_size
+        webpage = self.buckets[bucket_index]
+        while webpage:
+            if webpage.url == url:
+                self.cache.remove(webpage)
+                self.cache.insert(0, webpage)
+                return True
+            webpage = webpage.next
+        return False
+
+    # Access a page and update the cache so that it stores the most recently
+    # accessed N pages. This needs to be done with mostly O(1).
+    # |url|: The accessed URL
+    # |contents|: The contents of the URL
+    def access_page(self, url, contents):
+        assert type(url) == str
+        bucket_index = calculate_hash(url) % self.bucket_size
+        webpage = self.buckets[bucket_index]
+        # If it exists, length doesn't matter.
+        if self.update(url):
+            return
+        
+        new_webpage = Webpage(url, contents, self.buckets[bucket_index])
+        self.buckets[bucket_index] = new_webpage
+
+        if len(self.cache) >= self.max_cache:
+            self.cache.pop()
+        self.cache.insert(0, new_webpage)
+
+    # Return the URLs stored in the cache. The URLs are ordered in the order
+    # in which the URLs are mostly recently accessed.
+    def get_pages(self):
+        cache = self.cache
+        return [webpage.url for webpage in cache]
+
+
+def cache_test():
+    # Set the size of the cache to 4.
+    cache = Cache(4)
+
+    # Initially, no page is cached.
+    assert cache.get_pages() == []
+
+    # Access "a.com".
+    cache.access_page("a.com", "AAA")
+    # "a.com" is cached.
+    assert cache.get_pages() == ["a.com"]
+
+    # Access "b.com".
+    cache.access_page("b.com", "BBB")
+    # The cache is updated to:
+    #   (most recently accessed)<-- "b.com", "a.com" -->(least recently accessed)
+    assert cache.get_pages() == ["b.com", "a.com"]
+
+    # Access "c.com".
+    cache.access_page("c.com", "CCC")
+    # The cache is updated to:
+    #   (most recently accessed)<-- "c.com", "b.com", "a.com" -->(least recently accessed)
+    assert cache.get_pages() == ["c.com", "b.com", "a.com"]
+
+    # Access "d.com".
+    cache.access_page("d.com", "DDD")
+    # The cache is updated to:
+    #   (most recently accessed)<-- "d.com", "c.com", "b.com", "a.com" -->(least recently accessed)
+    assert cache.get_pages() == ["d.com", "c.com", "b.com", "a.com"]
+
+    # Access "d.com" again.
+    cache.access_page("d.com", "DDD")
+    # The cache is updated to:
+    #   (most recently accessed)<-- "d.com", "c.com", "b.com", "a.com" -->(least recently accessed)
+    assert cache.get_pages() == ["d.com", "c.com", "b.com", "a.com"]
+
+    # Access "a.com" again.
+    cache.access_page("a.com", "AAA")
+    # The cache is updated to:
+    #   (most recently accessed)<-- "a.com", "d.com", "c.com", "b.com" -->(least recently accessed)
+    assert cache.get_pages() == ["a.com", "d.com", "c.com", "b.com"]
+
+    cache.access_page("c.com", "CCC")
+    assert cache.get_pages() == ["c.com", "a.com", "d.com", "b.com"]
+    cache.access_page("a.com", "AAA")
+    assert cache.get_pages() == ["a.com", "c.com", "d.com", "b.com"]
+    cache.access_page("a.com", "AAA")
+    assert cache.get_pages() == ["a.com", "c.com", "d.com", "b.com"]
+
+    # Access "e.com".
+    cache.access_page("e.com", "EEE")
+    # The cache is full, so we need to remove the least recently accessed page "b.com".
+    # The cache is updated to:
+    #   (most recently accessed)<-- "e.com", "a.com", "c.com", "d.com" -->(least recently accessed)
+    assert cache.get_pages() == ["e.com", "a.com", "c.com", "d.com"]
+
+    # Access "f.com".
+    cache.access_page("f.com", "FFF")
+    # The cache is full, so we need to remove the least recently accessed page "c.com".
+    # The cache is updated to:
+    #   (most recently accessed)<-- "f.com", "e.com", "a.com", "c.com" -->(least recently accessed)
+    assert cache.get_pages() == ["f.com", "e.com", "a.com", "c.com"]
+
+    # Access "e.com".
+    cache.access_page("e.com", "EEE")
+    # The cache is updated to:
+    #   (most recently accessed)<-- "e.com", "f.com", "a.com", "c.com" -->(least recently accessed)
+    assert cache.get_pages() == ["e.com", "f.com", "a.com", "c.com"]
+
+    # Access "a.com".
+    cache.access_page("a.com", "AAA")
+    # The cache is updated to:
+    #   (most recently accessed)<-- "a.com", "e.com", "f.com", "c.com" -->(least recently accessed)
+    assert cache.get_pages() == ["a.com", "e.com", "f.com", "c.com"]
+
+    print("Tests passed!")
+
+
+if __name__ == "__main__":
+    cache_test()
